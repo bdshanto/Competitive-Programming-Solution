b51abdd2830826637927c3da753a4cfaa984d93b
bdshanto <hsib@yahoo.com>
2019-11-28 00:59:23 +0600

    ReverStringApp

---
+++ ReverseStringApp/ReverseStringApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ReverseStringApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input a string:\n");
+            string inputMsg = Console.ReadLine();
+            string reverseMsg = "";
+            for (int i = inputMsg.Length-1; i >= 0; i--)
+            {
+                reverseMsg += inputMsg[i];
+            }
+
+            Console.WriteLine(reverseMsg);
+        }
+    }
+}
