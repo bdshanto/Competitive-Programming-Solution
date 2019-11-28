2361fa73d077f870a2660ae96f09264a3e044605
bdshanto <hsib@yahoo.com>
2019-11-28 19:17:12 +0600

    Merge_Folder

---
+++ Competitive-Programming-Solution/ItemCountInArray/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ExceptionServices;
+
+namespace ItemCountInArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arr = {"1", "1", "2", "2", "2", "3", "3", "4", "5", "5", "4", "5", "6","7"};
+
+            List<string> arrTemp =new List<string>();
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = i+1; j < arr.Length; j++)
+                {
+                    if (  !arrTemp.Contains(arr[i]) )
+                    {
+                        /*Console.WriteLine(arr[i]);*/
+
+                        arrTemp.Add(arr[j]);
+
+                    }
+                }
+            }
+
+            foreach (var item in arrTemp)
+            {
+                Console.WriteLine(item);
+                
+            }
+        }
+          
+    }
+}
